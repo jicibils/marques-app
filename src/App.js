07ae9d20ae2d51c74983109befb4a559af6f0483
jicibils <jicibils@hotmail.com>
2019-10-11 02:43:47 -0300

    second

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RootRoutes from './routes/rootRoutes';
+
+import './App.scss';
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <RootRoutes />
+      </div>
+    );
+  }
+}
+
+export default App;
